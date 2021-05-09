e531652425ae5ae7da53fcad32321c8e12bd25cd
Jiseon-Park <79893752+Jiseon-Park@users.noreply.github.com>
2021-05-09 19:53:59 +0900

    Jump To Java

---
+++ JumpToJava/src/Test/TestString_0304.java
@@ -0,0 +1,21 @@
+package Test;
+/*
+ *  String a변수에 헬로우 자바 선언
+ *  자바 글자가 포함 되어 있는가
+ *  자바를 월드로 변경
+ *  헬까지 출력
+ *  올 대문자로 출력하기
+ */
+public class TestString_0304 {
+
+	public static void main(String[] args) {
+		String a = "Hello Java";
+		System.out.println(a.indexOf("Java"));
+		System.out.println(a.replaceAll("Java", "World"));
+		System.out.println(a.substring(0, 4));
+		System.out.println(a.toUpperCase());
+				
+
+	}
+
+}
---
+++ JumpToJava/src/Test/WhileTest_0403.java
@@ -0,0 +1,58 @@
+package Test;
+
+public class WhileTest_0403 {
+	public static void main(String[] args) {
+		/*
+		 * 열 번 찍어 안 넘어 가는 나무 없다
+		 */
+		int hit = 0;
+		while(hit < 10) {
+			hit++;
+			System.out.printf("나무를 %d번 찍었습니다. \n", hit);
+			if(hit == 10) {
+				System.out.println("나무가 넘어갑니다");
+			}
+		}
+		
+		/*
+		 * "돈을 받으면 커피를 줘라"
+		 */
+		int coffee = 10;
+		int money = 5000;
+		while(money > 0) {
+			System.out.println("돈을 냈으니 커피 드릴게요");
+			coffee--;
+			System.out.println("남은 커피 : " + coffee);
+			System.out.println();
+			if(coffee == 0) {
+				System.out.println("커피 품절입니다~~");
+				break;
+			}
+		}
+		/*
+		 * 1부터 10까지 홀수만을 출력
+		 */
+		int number = 0;
+		while(number < 10) {
+			number++;
+			if(number % 2 == 0) {
+				continue;
+			}
+			System.out.println(number);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
